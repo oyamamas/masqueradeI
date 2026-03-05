9c74904c2f4fc1b41b2d0cee03c56ab87a4ced9c
mas0yama <emirgamzaevsan@gmail.com>
2026-03-05 03:00:26 +0300

    trevorproxy idea now; ssh process ready;

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -1,13 +1,16 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+Copyright © 2026 oyama
 */
 package cmd
 
 import (
-	//"context"
-	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"strconv"
 	"strings"
+	"syscall"
+	"time"
 
 	//"golang.org/x/crypto/ssh"
 	//"net"
@@ -17,62 +20,95 @@ import (
 	//"golang.org/x/net/context"
 )
 
+type Tunnel struct {
+	PID              int
+	localPort        int
+	connectionString string
+}
+
 var (
 	SSHConnectionStrings []string
+	facadePort           int16
+	masqPort             int
 )
+
+func parseSSHConnectionString(connString string) (string, int) {
+	var address string = ""
+	var port int = 0
+	if len(strings.Split(connString, ":")) < 2 {
+		address = connString
+		port = 22
+	} else {
+		portSplit := strings.Split(connString, ":")
+		address = portSplit[0]
+		port, _ = strconv.Atoi(portSplit[1])
+	}
+	return address, port
+}
+
+func cleanUpSSHTunnels(tunnels []*Tunnel) {
+	for _, t := range tunnels {
+		if t.PID > 0 {
+			_ = syscall.Kill(t.PID, syscall.SIGTERM)
+			log.Printf("Остановлен ssh → (pid %d)", t.PID)
+		}
+	}
+}
+
+func spinUpSSHTunnels(connStrings []string) []*Tunnel {
+	args := []string{
+		"-D", strconv.Itoa(masqPort),
+		"-N", "-f",
+		"-o", "ServerAliveInterval=60",
+		"-o", "ServerAliveCountMax=3",
+		"-o", "ExitOnForwardFailure=yes",
+		"-o", "StrictHostKeyChecking=no",
+	}
+
+	masqPort++
+
+	var tunnels []*Tunnel
+	for _, connString := range connStrings {
+		address, port := parseSSHConnectionString(connString)
+		cmd := exec.Command("ssh", append(args, "-p", strconv.Itoa(port), address)...)
+
+		if err := cmd.Start(); err != nil {
+			log.Fatalln("AAAAAS")
+		}
+
+		time.Sleep(800 * time.Millisecond)
+
+		if cmd.Process == nil || cmd.Process.Pid == 0 {
+			log.Fatalln("AAAAAAAAAAASSSS")
+		}
+		tunnels = append(tunnels, &Tunnel{PID: cmd.Process.Pid, localPort: masqPort})
+	}
+	return tunnels
+}
 
 // proxyCmd represents the proxy command
 var proxyCmd = &cobra.Command{
 	Use:   "proxy",
 	Short: "Single socks5 <-> ssh tunnel",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Spinning up SSH Tunnels")
 
-		//
-		// ssh-agent here
-		//
+		// Check euid == 0
+		if os.Geteuid() != 0 {
+			log.Fatalln("Not enough privileges. Run with sudo/suid")
+		}
 
-		//sshConfig := &ssh.ClientConfig{
-		//	Config:          ssh.Config{},
-		//	User:            "",
-		//	Auth:            nil,
-		//	HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		//}
-		fmt.Println(parseSSHConnectionString(SSHConnectionStrings[0]))
+		// Setup initial masqPort
+		masqPort = 13370
 
-		fmt.Println("Starting SOCK5 listen server %")
-
-		//socksConfig := &socks5.Config{
-		//	Rules: socks5.PermitAll(),
-		//	Dial: func(ctx context.Context, network, addr string) (net.Conn, error) {
-		//		return nil, nil
-		//	},
-		//}
+		tunnels := spinUpSSHTunnels(SSHConnectionStrings)
+		defer cleanUpSSHTunnels(tunnels)
 
 	},
 }
 
-func parseSSHConnectionString(ConnectionString string) (string, string, int) {
-	UserNameSplited := strings.Split(ConnectionString, "@")
-	var user string = ""
-	var address string = ""
-	var port int = 0
-
-	if len(strings.Split(UserNameSplited[1], ":")) < 2 {
-		user = UserNameSplited[0]
-		address = UserNameSplited[1]
-		port = 22
-	} else {
-		user = UserNameSplited[0]
-		PortSplited := strings.Split(UserNameSplited[1], ":")
-		address = PortSplited[0]
-		port, _ = strconv.Atoi(PortSplited[1])
-	}
-	return user, address, port
-}
-
 func init() {
 	rootCmd.AddCommand(proxyCmd)
+	proxyCmd.Flags().Int16VarP(&facadePort, "port", "p", 1337, "facade (listen) port")
 	proxyCmd.Flags().StringSliceVarP(&SSHConnectionStrings, "ssh", "s", []string{}, "sshstring")
 	// Here you will define your flags and configuration settings.
 
